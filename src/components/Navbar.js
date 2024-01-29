f75e5789d600baeb53246e06fce60cc8c5b7ed17
daradara <devdaradara@gmail.com>
2024-01-29 16:41:07 +0900

    style: 나브바 스타일 수정

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import BuddyFriendsLogo from "../assets/icons/BuddyFriendsLogo.png";
@@ -8,18 +8,24 @@ const NavContainer = styled.nav`
   justify-content: space-between;
   align-items: center;
   padding: 0px 20px;
-  background-color: #F6BD60;
-  font-family: 'SCDream5';
+  background-color: #f6bd60;
+  font-family: "SCDream5";
   font-size: 20px;
 `;
 
 const TextLink = styled(Link)`
   text-decoration: none;
-`
+`;
 
 const NavItems = styled.div`
   display: flex;
   align-items: center;
+`;
+
+const NavPageItem = styled.div`
+  margin: 0 200px;
+  cursor: pointer;
+  color: #000;
 `;
 
 const NavItem = styled.div`
@@ -31,30 +37,52 @@ const NavItem = styled.div`
 const Navber = () => {
   const [userInfo, setUserInfo] = useState("사용자1");
 
+  const handleLogout = () => {
+    setUserInfo(null);
+  };
+
   return (
     <NavContainer>
       <NavItems>
         <TextLink to="/">
-          <img src={BuddyFriendsLogo} alt={'Logo'} style={{ width: 'auto', height: '60px', marginRight: '20px', marginTop: '5px'}} />
+          <img
+            src={BuddyFriendsLogo}
+            alt={"Logo"}
+            style={{
+              width: "auto",
+              height: "70px",
+              marginRight: "20px",
+              marginTop: "5px",
+            }}
+          />
         </TextLink>
-        <TextLink to="/">
-          <NavItem>돌봄글 작성</NavItem>
+        <TextLink to="/carepost">
+          <NavPageItem>게시글 작성</NavPageItem>
         </TextLink>
-        <TextLink to="/">
-          <NavItem>갤러리</NavItem>
+        <TextLink to="/gallery">
+          <NavPageItem>갤러리</NavPageItem>
+        </TextLink>
+        <TextLink to="/check">
+          <NavPageItem>돌봄 진행 현황</NavPageItem>
         </TextLink>
       </NavItems>
       <NavItems>
-        {userInfo!==null ? (
+        {userInfo !== null ? (
           <>
-            <NavItem>{userInfo.name}</NavItem>
-            <NavItem>LogOut</NavItem>
+            <TextLink to="/mypage">
+              <NavItem>{userInfo}</NavItem>
+            </TextLink>
+            <NavItem onClick={handleLogout}>LogOut</NavItem>
           </>
         ) : (
-          <TextLink to="/login">
-            <NavItem>LogIn</NavItem>
-            <NavItem>SignUp</NavItem>
-          </TextLink>
+          <>
+            <TextLink to="/login">
+              <NavItem>LogIn</NavItem>
+            </TextLink>
+            <TextLink to="/signup">
+              <NavItem>SignUp</NavItem>
+            </TextLink>
+          </>
         )}
       </NavItems>
     </NavContainer>
