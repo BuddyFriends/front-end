bb77ecb763d4ae91565b70478d7422129f19777a
dalpaeng6 <20220876@duksung.ac.kr>
2024-01-30 22:31:42 +0900

    main페이지

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -48,6 +48,62 @@ const Name = styled.div`
   }
 `;
 
+const CareContainer = styled.div`
+  text-align: center;
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+`;
+
+const Underline = styled.div`
+  height: 2px;
+  width: 50px;
+  background-color: #333;
+  margin: 10px auto;
+`;
+
+const Image2Container = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const CircleImage = styled.img`
+  width: 300px;
+  height: 300px;
+  border-radius: 50%;
+  opacity: 0.7;
+  &:hover {
+    opacity: 1;
+  }
+  margin: 0 10px;
+`;
+
+const CircleContainer = styled.div`
+  text-align: center;
+  margin: 50px;
+`;
+
+const OrangeCircle = styled.div`
+  width: 300px;
+  height: 300px;
+  background-color: #FF881A;
+  opacity: 0.7;
+  border-radius: 50%;
+  margin: 0 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  &:hover {
+    opacity: 1;
+  }
+`;
+
+const TextInCircle = styled.span`
+  color: black;
+  font-size: 24px;
+`;
 
 function MainPage() {
   const navigate = useNavigate();
@@ -57,6 +113,7 @@ function MainPage() {
   };
 
   return (
+    <div>
     <MainContainer>
       <ImageContainer>
         <Image src="/images/maincat.png" alt="cat" />
@@ -67,6 +124,24 @@ function MainPage() {
         <button onClick={navigateTocarepost}>돌봄신청</button>
       </TextContainer>
     </MainContainer>
+    <CareContainer>
+    <Title>Your Title</Title>
+    <Underline />
+    <Image2Container>
+      <CircleContainer>
+        <CircleImage src="/images/dog.png" alt="dog" />
+      </CircleContainer>
+      <CircleContainer>
+        <CircleImage src="/images/cat.png" alt="cat" />
+      </CircleContainer>
+      <CircleContainer>
+        <OrangeCircle>
+          <TextInCircle>other</TextInCircle>
+        </OrangeCircle>
+      </CircleContainer>
+    </Image2Container>
+    </CareContainer>
+    </div>
   );
 }
 
